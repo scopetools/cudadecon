56ae207431888472b4ca45b6c470afe9f404a84f
linshaova <shaomumu@gmail.com>
2014-07-23 16:08:34 +0000

    Added explicit exception handling in main(), so that exceptions won't cause crash on Windows
    git-svn-id: https://subversion.int.janelia.org/betziglab/projects@23 9dab5866-824d-490b-b8a1-734f56929cb1

--- linearDecon.cpp
+++ linearDecon.cpp
@@ -3,6 +3,7 @@
 namespace po = boost::program_options;
 
 #include "linearDecon.h"
+#include <exception>
 
 std::complex<float> otfinterpolate(std::complex<float> * otf, float kx, float ky, float kz, int nzotf, int nrotf)
 // Use sub-pixel coordinates (kx,ky,kz) to linearly interpolate a rotationally-averaged 3D OTF ("otf").
@@ -146,17 +147,21 @@ int main(int argc, char *argv[])
 
 /* Parse commandline option */
   po::variables_map varsmap;
+  try {
+    store(po::command_line_parser(argc, argv).
+          options(progopts).positional(p).run(), varsmap);
+    if (varsmap.count("help")) {
+      std::cout << progopts << "\n";
+      return 0;
+    }
 
-  store(po::command_line_parser(argc, argv).
-        options(progopts).positional(p).run(), varsmap);
+    notify(varsmap);
 
-  if (varsmap.count("help")) {
-    std::cout << progopts << "\n";
+  }
+  catch (std::exception &e) {
+    std::cout << "\n!!Error occurred: " << e.what();
     return 0;
   }
-
-  notify(varsmap);
-
   // Gather all files in 'datafolder' and matching the file name pattern:
   std::vector< std::string > all_matching_files = 
     gatherMatchingFiles(datafolder, filenamePattern);
@@ -177,193 +182,200 @@ int main(int argc, char *argv[])
   cudaGetDeviceProperties(&deviceProp, 0);
 
   // Loop over all matching input TIFFs:
-  for (std::vector<std::string>::iterator it=all_matching_files.begin();
-       it != all_matching_files.end(); it++) {
+  try {
+    for (std::vector<std::string>::iterator it=all_matching_files.begin();
+         it != all_matching_files.end(); it++) {
 
-    std::cout<< *it << std::endl;
-    raw_image.assign(it->c_str());
+      std::cout<< *it << std::endl;
+      raw_image.assign(it->c_str());
 
-    // If it's the first input file, initialize a bunch including:
-    // 1. crop image to make dimensions nice factorizable numbers
-    // 2. calculate deskew parameters, new X dimensions
-    // 3. calculate rotation matrix
-    // 4. create FFT plans
-    // 5. transfer constants into GPU device constant memory
-    // 6. make 3D OTF array in device memory
-    if (it == all_matching_files.begin()) {
-      unsigned nx = raw_image.width();
-      unsigned ny = raw_image.height();
-      unsigned nz = raw_image.depth();
+      // If it's the first input file, initialize a bunch including:
+      // 1. crop image to make dimensions nice factorizable numbers
+      // 2. calculate deskew parameters, new X dimensions
+      // 3. calculate rotation matrix
+      // 4. create FFT plans
+      // 5. transfer constants into GPU device constant memory
+      // 6. make 3D OTF array in device memory
+      if (it == all_matching_files.begin()) {
+        unsigned nx = raw_image.width();
+        unsigned ny = raw_image.height();
+        unsigned nz = raw_image.depth();
 
-      printf("Original image size: nz=%d, ny=%d, nx=%d\n", nz, ny, nx);
+        printf("Original image size: nz=%d, ny=%d, nx=%d\n", nz, ny, nx);
 
-      if (RL_iters>0) {
-        new_ny = findOptimalDimension(ny);
-        if (new_ny != ny) {
-          printf("new ny=%d\n", new_ny);
-          bCrop = true;
-        }
-
-        new_nz = findOptimalDimension(nz);
-        if (new_nz != nz) {
-          printf("new nz=%d\n", new_nz);
-          bCrop = true;
-        }
-
-        // only if no deskewing is happening do we want to change image width here
-        new_nx = nx;
-        if (!fabs(deskewAngle) > 0.0) {
-          new_nx = findOptimalDimension(nx);
-          if (new_nx != nx) {
-            printf("new nx=%d\n", new_nx);
+        if (RL_iters>0) {
+          new_ny = findOptimalDimension(ny);
+          if (new_ny != ny) {
+            printf("new ny=%d\n", new_ny);
             bCrop = true;
           }
+
+          new_nz = findOptimalDimension(nz);
+          if (new_nz != nz) {
+            printf("new nz=%d\n", new_nz);
+            bCrop = true;
+          }
+
+          // only if no deskewing is happening do we want to change image width here
+          new_nx = nx;
+          if (!fabs(deskewAngle) > 0.0) {
+            new_nx = findOptimalDimension(nx);
+            if (new_nx != nx) {
+              printf("new nx=%d\n", new_nx);
+              bCrop = true;
+            }
+          }
         }
+        else {
+          new_nx = nx;
+          new_ny = ny;
+          new_nz = nz;
+        }
+
+        // Load OTF (assuming 3D rotationally averaged OTF):
+        complexOTF.assign(otffiles.c_str());
+        unsigned nr_otf = complexOTF.height();
+        unsigned nz_otf = complexOTF.width() / 2;
+        dkr_otf = 1/((nr_otf-1)*2 * dr_psf);
+        dkz_otf = 1/(nz_otf * dz_psf);
+
+        // Construct deskew matrix:
+        deskewedXdim = new_nx;
+        if (fabs(deskewAngle) > 0.0) {
+          if (deskewAngle <0) deskewAngle += 180.;
+          deskewFactor = cos(deskewAngle * M_PI/180.) * imgParams.dz / imgParams.dr;
+          if (outputWidth ==0)
+            deskewedXdim += floor(new_nz * imgParams.dz * 
+                                  fabs(cos(deskewAngle * M_PI/180.)) / imgParams.dr);
+          // TODO: sometimes deskewedXdim calc'ed this way is too large
+          else
+            deskewedXdim = outputWidth; // use user-provided output width if available
+
+          deskewedXdim = findOptimalDimension(deskewedXdim);
+
+          // update z step size:
+          imgParams.dz *= sin(deskewAngle * M_PI/180.);
+
+          printf("deskewFactor=%f, new nx=%d\n", deskewFactor, deskewedXdim);
+
+          if (bSaveDeskewedRaw) {
+            raw_deskewed.assign(deskewedXdim, new_ny, new_nz);
+            makeDeskewedDir("Deskewed");
+          }
+        }
+
+        // Construct rotation matrix:
+        if (fabs(rotationAngle) > 0.0) {
+          rotMatrix.resize(4*sizeof(float));
+          rotationAngle *= M_PI/180;
+          float stretch = imgParams.dr / imgParams.dz;
+          float *p = (float *)rotMatrix.getPtr();
+          p[0] = cos(rotationAngle) * stretch;
+          p[1] = sin(rotationAngle) * stretch;
+          p[2] = -sin(rotationAngle);
+          p[3] = cos(rotationAngle);
+        }
+
+        if (wiener >0) {
+          raw_imageFFT.assign(deskewedXdim+2, new_ny, new_nz);
+
+          if (!fftwf_init_threads()) { /* one-time initialization required to use threads */
+            printf("Error returned by fftwf_init_threads()\n");
+          }
+
+          fftwf_plan_with_nthreads(8);
+          rfftplan = fftwf_plan_dft_r2c_3d(new_nz, new_ny, deskewedXdim,
+                                           raw_image.data(),
+                                           (fftwf_complex *) raw_imageFFT.data(),
+                                           FFTW_ESTIMATE);
+          rfftplan_inv = fftwf_plan_dft_c2r_3d(new_nz, new_ny, deskewedXdim,
+                                               (fftwf_complex *) raw_imageFFT.data(),
+                                               raw_image.data(),
+                                               FFTW_ESTIMATE);
+        }
+        else if (RL_iters>0) {
+          // Create reusable cuFFT plans
+          cufftResult cuFFTErr = cufftPlan3d(&rfftplanGPU, new_nz, new_ny, deskewedXdim, CUFFT_R2C);
+          if (cuFFTErr != CUFFT_SUCCESS) {
+            std::cout << "Error code: " << cuFFTErr << std::endl;
+            throw std::runtime_error("cufftPlan3d() r2c failed.");
+          }
+          cuFFTErr = cufftPlan3d(&rfftplanInvGPU, new_nz, new_ny, deskewedXdim, CUFFT_C2R);
+          if (cuFFTErr != CUFFT_SUCCESS) {
+            std::cout << "Error code: " << cuFFTErr << std::endl;
+            throw std::runtime_error("cufftPlan3d() c2r failed.");
+          }
+        }
+
+        dkx = 1.0/(imgParams.dr * deskewedXdim);
+        dky = 1.0/(imgParams.dr * new_ny);
+        dkz = 1.0/(imgParams.dz * new_nz);
+        rdistcutoff = 2*NA/(imgParams.wave); // lateral resolution limit in 1/um
+
+        // transfer a bunch of constants to device, including OTF array:
+        float eps = std::numeric_limits<float>::epsilon();
+        transferConstants(deskewedXdim, new_ny, new_nz,
+                          complexOTF.height(), complexOTF.width()/2,
+                          dkx/dkr_otf, dky/dkr_otf, dkz/dkz_otf,
+                          eps, complexOTF.data());
+
+        // make a 3D interpolated OTF array:
+        d_interpOTF.resize(new_nz * new_ny * (deskewedXdim+2) * sizeof(float));
+        makeOTFarray(d_interpOTF, deskewedXdim, new_ny, new_nz);
+      } // if (it == all_matching_files.begin())
+
+      if (bCrop) {
+        raw_image.crop(0, 0, 0, 0, new_nx-1, new_ny-1, new_nz-1, 0);
+        // If deskew is to happen, it'll be performed inside RichardsonLucy_GPU() on GPU;
+        // but here raw data's x dimension is still just "new_nx"
+      }
+
+      if (wiener >0) { // plain 1-step Wiener filtering
+        raw_image -= background;
+        fftwf_execute_dft_r2c(rfftplan, raw_image.data(), (fftwf_complex *) raw_imageFFT.data());
+
+        wienerfilter(raw_imageFFT, 
+                     dkx, dky, dkz,
+                     complexOTF,
+                     dkr_otf, dkz_otf,
+                     rdistcutoff, wiener);
+
+        fftwf_execute_dft_c2r(rfftplan_inv, (fftwf_complex *) raw_imageFFT.data(), raw_image.data());
+        raw_image /= raw_image.size();
+      }
+      else if (RL_iters) {
+        // if (bCPU) {
+        //   raw_image -= background;
+        //   raw_image.max(0.f); // background subtraction earlier could have caused negative pixels
+        //   RichardsonLucy(raw_image, imgParams.dr, imgParams.dz,
+        //                  complexOTF, dkr_otf, dkz_otf,
+        //                  rdistcutoff, RL_iters,
+        //                  rfftplan, rfftplan_inv, raw_imageFFT);
+        // }
+        // else
+        RichardsonLucy_GPU(raw_image, background, d_interpOTF, RL_iters,
+                           deskewFactor, deskewedXdim, extraShift, napodize, rotMatrix,
+                           rfftplanGPU, rfftplanInvGPU, raw_deskewed, &deviceProp);
+      }
+      else if (rotMatrix.getSize()) {// do only rotation
+        std::cout << rotMatrix.getSize() << std::endl;
+        RichardsonLucy_GPU(raw_image, background, d_interpOTF, RL_iters,
+                           deskewFactor, deskewedXdim, extraShift, napodize, rotMatrix,
+                           rfftplanGPU, rfftplanInvGPU, raw_deskewed, &deviceProp);
       }
       else {
-        new_nx = nx;
-        new_ny = ny;
-        new_nz = nz;
+        std::cerr << "Nothing is performed\n";
+        break;
       }
 
-      // Load OTF (assuming 3D rotationally averaged OTF):
-      complexOTF.assign(otffiles.c_str());
-      unsigned nr_otf = complexOTF.height();
-      unsigned nz_otf = complexOTF.width() / 2;
-      dkr_otf = 1/((nr_otf-1)*2 * dr_psf);
-      dkz_otf = 1/(nz_otf * dz_psf);
-
-      // Construct deskew matrix:
-      deskewedXdim = new_nx;
-      if (fabs(deskewAngle) > 0.0) {
-        if (deskewAngle <0) deskewAngle += 180.;
-        deskewFactor = cos(deskewAngle * M_PI/180.) * imgParams.dz / imgParams.dr;
-        if (outputWidth ==0)
-          deskewedXdim += floor(new_nz * imgParams.dz * 
-                                fabs(cos(deskewAngle * M_PI/180.)) / imgParams.dr); // TODO /4.
-        else
-          deskewedXdim = outputWidth; // use user-provided output width if available
-
-        deskewedXdim = findOptimalDimension(deskewedXdim);
-
-        // update z step size:
-        imgParams.dz *= sin(deskewAngle * M_PI/180.);
-
-        printf("deskewFactor=%f, new nx=%d\n", deskewFactor, deskewedXdim);
-
-        if (bSaveDeskewedRaw) {
-          raw_deskewed.assign(deskewedXdim, new_ny, new_nz);
-          makeDeskewedDir("Deskewed");
-        }
-      }
-
-      // Construct rotation matrix:
-      if (fabs(rotationAngle) > 0.0) {
-        rotMatrix.resize(4*sizeof(float));
-        rotationAngle *= M_PI/180;
-        float stretch = imgParams.dr / imgParams.dz;
-        float *p = (float *)rotMatrix.getPtr();
-        p[0] = cos(rotationAngle) * stretch;
-        p[1] = sin(rotationAngle) * stretch;
-        p[2] = -sin(rotationAngle);
-        p[3] = cos(rotationAngle);
-      }
-
-      if (wiener >0) {
-        raw_imageFFT.assign(deskewedXdim+2, new_ny, new_nz);
-
-        if (!fftwf_init_threads()) { /* one-time initialization required to use threads */
-          printf("Error returned by fftwf_init_threads()\n");
-        }
-
-        fftwf_plan_with_nthreads(8);
-        rfftplan = fftwf_plan_dft_r2c_3d(new_nz, new_ny, deskewedXdim,
-                                         raw_image.data(),
-                                         (fftwf_complex *) raw_imageFFT.data(),
-                                         FFTW_ESTIMATE);
-        rfftplan_inv = fftwf_plan_dft_c2r_3d(new_nz, new_ny, deskewedXdim,
-                                             (fftwf_complex *) raw_imageFFT.data(),
-                                             raw_image.data(),
-                                             FFTW_ESTIMATE);
-      }
-      else if (RL_iters>0) {
-        // Create reusable cuFFT plans
-        cufftResult cuFFTErr = cufftPlan3d(&rfftplanGPU, new_nz, new_ny, deskewedXdim, CUFFT_R2C);
-        if (cuFFTErr != CUFFT_SUCCESS) {
-          std::cout << "Error code: " << cuFFTErr << std::endl;
-          throw std::runtime_error("cufftPlan3d() r2c failed.");
-        }
-        cuFFTErr = cufftPlan3d(&rfftplanInvGPU, new_nz, new_ny, deskewedXdim, CUFFT_C2R);
-        if (cuFFTErr != CUFFT_SUCCESS) {
-          std::cout << "Error code: " << cuFFTErr << std::endl;
-          throw std::runtime_error("cufftPlan3d() c2r failed.");
-        }
-      }
-
-      dkx = 1.0/(imgParams.dr * deskewedXdim);
-      dky = 1.0/(imgParams.dr * new_ny);
-      dkz = 1.0/(imgParams.dz * new_nz);
-      rdistcutoff = 2*NA/(imgParams.wave); // lateral resolution limit in 1/um
-
-      // transfer a bunch of constants to device, including OTF array:
-      float eps = std::numeric_limits<float>::epsilon();
-      transferConstants(deskewedXdim, new_ny, new_nz,
-                        complexOTF.height(), complexOTF.width()/2,
-                        dkx/dkr_otf, dky/dkr_otf, dkz/dkz_otf,
-                        eps, complexOTF.data());
-
-      // make a 3D interpolated OTF array:
-      d_interpOTF.resize(new_nz * new_ny * (deskewedXdim+2) * sizeof(float));
-      makeOTFarray(d_interpOTF, deskewedXdim, new_ny, new_nz);
-    } // if (it == all_matching_files.begin())
-
-    if (bCrop) {
-      raw_image.crop(0, 0, 0, 0, new_nx-1, new_ny-1, new_nz-1, 0);
-      // If deskew is to happen, it'll be performed inside RichardsonLucy_GPU() on GPU;
-      // but here raw data's x dimension is still just "new_nx"
-    }
-
-    if (wiener >0) { // plain 1-step Wiener filtering
-      raw_image -= background;
-      fftwf_execute_dft_r2c(rfftplan, raw_image.data(), (fftwf_complex *) raw_imageFFT.data());
-
-      wienerfilter(raw_imageFFT, 
-                   dkx, dky, dkz,
-                   complexOTF,
-                   dkr_otf, dkz_otf,
-                   rdistcutoff, wiener);
-
-      fftwf_execute_dft_c2r(rfftplan_inv, (fftwf_complex *) raw_imageFFT.data(), raw_image.data());
-      raw_image /= raw_image.size();
-    }
-    else if (RL_iters) {
-      // if (bCPU) {
-      //   raw_image -= background;
-      //   raw_image.max(0.f); // background subtraction earlier could have caused negative pixels
-      //   RichardsonLucy(raw_image, imgParams.dr, imgParams.dz,
-      //                  complexOTF, dkr_otf, dkz_otf,
-      //                  rdistcutoff, RL_iters,
-      //                  rfftplan, rfftplan_inv, raw_imageFFT);
-      // }
-      // else
-      RichardsonLucy_GPU(raw_image, background, d_interpOTF, RL_iters,
-                         deskewFactor, deskewedXdim, extraShift, napodize, rotMatrix,
-                         rfftplanGPU, rfftplanInvGPU, raw_deskewed, &deviceProp);
-    }
-    else if (rotMatrix.getSize()) {// do only rotation
-      std::cout << rotMatrix.getSize() << std::endl;
-      RichardsonLucy_GPU(raw_image, background, d_interpOTF, RL_iters,
-                         deskewFactor, deskewedXdim, extraShift, napodize, rotMatrix,
-                         rfftplanGPU, rfftplanInvGPU, raw_deskewed, &deviceProp);
-    }
-    else {
-      std::cerr << "Nothing is performed\n";
-      break;
-    }
-
-    raw_image.save(makeOutputFilePath(*it).c_str());
-    if (bSaveDeskewedRaw)
-      raw_deskewed.save(makeOutputFilePath(*it, "Deskewed", "_deskewed").c_str());
-  } // iteration over all_matching_files
+      raw_image.save(makeOutputFilePath(*it).c_str());
+      if (bSaveDeskewedRaw)
+        raw_deskewed.save(makeOutputFilePath(*it, "Deskewed", "_deskewed").c_str());
+    } // iteration over all_matching_files
+  } // try {} block
+  catch (std::exception &e) {
+    std::cerr << "\n!!Error occurred: " << e.what() << std::endl;
+    return 0;
+  }
   return 0; 
 }
