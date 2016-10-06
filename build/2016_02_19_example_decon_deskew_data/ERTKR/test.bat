5f6ae7e9f9a217c947e8bd1ce3336bd66344679d
dmilkie <dmilkie@gmail.com>
2016-10-06 19:30:31 +0000

    Fixed OTF path.
    git-svn-id: https://subversion.int.janelia.org/betziglab/projects@66 9dab5866-824d-490b-b8a1-734f56929cb1

--- build/2016_02_19_example_decon_deskew_data/ERTKR/test.bat
+++ build/2016_02_19_example_decon_deskew_data/ERTKR/test.bat
@@ -1,2 +1,2 @@
 ..\..\radialft.exe ..\mbPSF_560_NAp5nap42_z100nm.tif ..\mbOTF_560_NAp5nap42_z100nm.tif --nocleanup --fixorigin 10
-..\..\cudaDeconv.exe -a -z .36 -D 32.8 -R 32.8 -i 10 -M 0 0 1 -S . sample_scan_560_20ms_zp36_cell1 ..\mbPSF_560_NAp5nap42_z100nm.tif
+..\..\cudaDeconv.exe %1 -z .36 -D 32.8 -R 32.8 -i 10 -M 0 0 1 -S . sample_scan_560_20ms_zp36_cell1 ..\mbOTF_560_NAp5nap42_z100nm.tif
