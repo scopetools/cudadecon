3d8b590ea9ab569a4a9a9c9d0e3a446133e27fed
dmilkie <dmilkie@gmail.com>
2016-11-04 13:55:47 +0000

    Fixed a problem when reporting the number of seconds per image processed when images are skipped.  Also added (hopefully) a TIFF tag for image description that will show the command line used.
    git-svn-id: https://subversion.int.janelia.org/betziglab/projects@78 9dab5866-824d-490b-b8a1-734f56929cb1

--- build/Makeit.bat
+++ build/Makeit.bat
@@ -1,1 +1,1 @@
-nmake && copy cudaDeconv.exe E:\CudaDecon\build /Y && copy ..\* E:\CudaDecon && copy cudaDeconv.exe "\\dm11\betziglab\Tool_Codes\Decon\GPU_decon\build\" /Y
+nmake cudaDeconv otfviewer radialft libcudaDeconv && copy cudaDeconv.exe E:\CudaDecon\build /Y && copy ..\* E:\CudaDecon && copy cudaDeconv.exe "\\dm11\betziglab\Tool_Codes\Decon\GPU_decon\build\" /Y
