2096c8fc93287b72e0e6aaf2f0387793bc4f6d70
dmilkie <dmilkie@gmail.com>
2017-02-08 18:15:44 +0000

    Load full tiff file into memory and process it via a stream.  This should improve image load time over the network.
    git-svn-id: https://subversion.int.janelia.org/betziglab/projects@89 9dab5866-824d-490b-b8a1-734f56929cb1

--- build/Makeit.bat
+++ build/Makeit.bat
@@ -1,1 +1,4 @@
-nmake && copy *.exe E:\CudaDecon\build /Y && copy ..\* E:\CudaDecon && copy *.exe "\\dm11\betziglab\Tool_Codes\Decon\GPU_decon\build\" /Y
+@Echo:
+@ nmake && copy *.exe E:\CudaDecon\build /Y && copy ..\* E:\CudaDecon && copy *.exe "\\dm11\betziglab\Tool_Codes\Decon\GPU_decon\build\" /Y
+@ call mediarunner.bat %windir%\media\tada.wav
+@Echo:
