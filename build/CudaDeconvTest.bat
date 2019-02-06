4bf010aa21c934000c9c4798ad453a991c41b66c
dmilkie <dmilkie@gmail.com>
2019-02-06 21:29:27 +0000

    Quote in wrong place. grrr.
    git-svn-id: https://subversion.int.janelia.org/betziglab/projects@102 9dab5866-824d-490b-b8a1-734f56929cb1

--- build/CudaDeconvTest.bat
+++ build/CudaDeconvTest.bat
@@ -1,5 +1,5 @@
 REM ****OTFGEN****
-radialft.exe .\2016_02_19_example_decon_deskew_data\mbPSF_560_NAp5nap42_z100nm.tif" .\2016_02_19_example_decon_deskew_data\mbOTF_560_NAp5nap42_z100nm.tif --nocleanup --fixorigin 10
+radialft.exe .\2016_02_19_example_decon_deskew_data\mbPSF_560_NAp5nap42_z100nm.tif .\2016_02_19_example_decon_deskew_data\mbOTF_560_NAp5nap42_z100nm.tif --nocleanup --fixorigin 10
 
 REM ****DECONV****
 cudaDeconv -z .36 -D 32.8 -R 32.8 -i 15 -M 0 0 1 -S --input-dir .\2016_02_19_example_decon_deskew_data\ERTKR --filename-pattern sample_scan_560 --otf-file .\2016_02_19_example_decon_deskew_data\mbOTF_560_NAp5nap42_z100nm.tif --NoBleachCorrection 
