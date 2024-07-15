6d4e32a0edc8f3dcd666ccbe5b41391664931cf6
Talley Lambert <talley.lambert@gmail.com>
2024-07-15 17:29:35 -0400

    uncomment camcor stuff

--- src/llspy.cpp
+++ src/llspy.cpp
@@ -42,12 +42,12 @@ extern "C" {
                        float * const result,
                        const float * affMat);
 
-  // LLSPY_API int camcor_interface_init(int nx, int ny, int nz,
-  //                      const float * const camparam);
+  LLSPY_API int camcor_interface_init(int nx, int ny, int nz,
+                       const float * const camparam);
 
-  // LLSPY_API int camcor_interface(const unsigned short * const raw_data,
-  //                      int nx, int ny, int nz,
-  //                      unsigned short * const result);
+  LLSPY_API int camcor_interface(const unsigned short * const raw_data,
+                       int nx, int ny, int nz,
+                       unsigned short * const result);
 
   LLSPY_API void cuda_reset();
 
@@ -66,29 +66,29 @@ void cuda_reset()
   cudaDeviceReset();
 }
 
-// int camcor_interface_init(int nx, int ny, int nz,
-//                      const float * const camparam)
-// {
-//   CImg<> h_camparam(camparam, nx, ny, 3);
-//   setupConst(nx, ny, nz);
-//   setupCamCor(nx, ny, h_camparam.data());
-//   return 1;
-// }
+int camcor_interface_init(int nx, int ny, int nz,
+                     const float * const camparam)
+{
+  CImg<> h_camparam(camparam, nx, ny, 3);
+  setupConst(nx, ny, nz);
+  setupCamCor(nx, ny, h_camparam.data());
+  return 1;
+}
 
 
-// int camcor_interface(const unsigned short * const raw_data,
-//                      int nx, int ny, int nz,
-//                      unsigned short * const result)
-// {
-//   CImg<unsigned short> input(raw_data, nx, ny, nz);
-//   CImg<unsigned> raw_image(input);
-//   GPUBuffer d_correctedResult(nx * ny * nz * sizeof(unsigned short), 0, false);
-//   setupData(nx, ny, nz, raw_image.data());
-//   camcor_GPU(nx, ny, nz, d_correctedResult);
-//   //transfer result back to host
-//   cudaMemcpy(result, d_correctedResult.getPtr(), nx * ny * nz * sizeof(unsigned short), cudaMemcpyDeviceToHost);
-//   return 1;
-// }
+int camcor_interface(const unsigned short * const raw_data,
+                     int nx, int ny, int nz,
+                     unsigned short * const result)
+{
+  CImg<unsigned short> input(raw_data, nx, ny, nz);
+  CImg<unsigned> raw_image(input);
+  GPUBuffer d_correctedResult(nx * ny * nz * sizeof(unsigned short), 0, false);
+  setupData(nx, ny, nz, raw_image.data());
+  camcor_GPU(nx, ny, nz, d_correctedResult);
+  //transfer result back to host
+  cudaMemcpy(result, d_correctedResult.getPtr(), nx * ny * nz * sizeof(unsigned short), cudaMemcpyDeviceToHost);
+  return 1;
+}
 
 
 
