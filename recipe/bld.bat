54b510a80791a3444e7903afc6f20ae6b89b5669
Talley Lambert <talley.lambert@gmail.com>
2024-07-15 20:02:23 -0400

    remove cudaxx

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,6 @@
 mkdir cmake_build
 cd cmake_build
 
-:: Handle external CTK in CUDA 11 builds
-if defined CUDA_PATH (
-    SET CUDACXX=%CUDA_PATH%\bin\nvcc.exe
-)
-
 cmake -G Ninja ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
